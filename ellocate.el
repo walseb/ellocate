88e0ebf12716f8d0dfe2852faff017ffd37af50e
Sebastian Wålinder <s.walinder@gmail.com>
2019-07-24 15:03:26 +0200

    Added license, improved readme

--- ellocate.el
+++ ellocate.el
@@ -4,7 +4,20 @@
 ;; URL: https://github.com/walseb/ellocate
 ;; Version: 1.0
 ;; Package-Requires: ((emacs "24.4") (s "1.12.0") (ivy "0.11.0"))
-;; Keywords: extensions
+;; Keywords: matching
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
